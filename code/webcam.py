2fc6ba5a4981b0dc7523c7479d11e71e2e288310
Alberto Ruiz <aruiz@um.es>
2018-01-30 17:45:59 +0100

    fix waitKey

--- code/webcam.py
+++ code/webcam.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 
-import cv2   as cv
+import cv2 as cv
 
 cap = cv.VideoCapture(0)
 
-while(cv.waitKey(1) != 27):
+while(cv.waitKey(1) & 0xFF != 27):
     ret, frame = cap.read()
-    
     cv.imshow('webcam',frame)
 
 cv.destroyAllWindows()
