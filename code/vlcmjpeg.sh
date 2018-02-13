87a7960795530de59bd99362a4801346ff22a3ba
Alberto Ruiz <aruiz@um.es>
2018-02-13 15:08:36 +0100

    ampliado colorseg

--- code/vlcmjpeg.sh
+++ code/vlcmjpeg.sh
@@ -1,7 +1,11 @@
 # based on
 # https://gist.github.com/marc-hanheide/7b3557f487f1353b2b7c
 
+# run first vlc url to accept certificates
+# add -q when it works
+
 PORT=${2:-8090}
 echo $PORT
 
-DISPLAY=:0 cvlc -q --no-audio $1 --sout "#transcode{vcodec=MJPG}:standard{access=http,mux=mpjpeg,dst=:$PORT}" --sout-http-mime="multipart/x-mixed-replace;boundary=--7b3cc56e5f51db803f790dad720ed50a"
+DISPLAY=:0 cvlc --no-audio $1 --sout "#transcode{vcodec=MJPG}:standard{access=http,mux=mpjpeg,dst=:$PORT}" --sout-http-mime="multipart/x-mixed-replace;boundary=--7b3cc56e5f51db803f790dad720ed50a"
+
