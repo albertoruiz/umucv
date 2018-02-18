e3500a1ae3d332805f829b2adc98333146850306
Alberto Ruiz <aruiz@um.es>
2018-02-18 20:01:17 +0100

    pause

--- code/deque.py
+++ code/deque.py
@@ -4,11 +4,8 @@
 
 import cv2         as cv
 import numpy       as np
-import argparse
-from umucv.stream  import mkStream
-from umucv.util    import sourceArgs
-from collections   import deque
-
+from umucv.stream  import autoStream
+from collections   import  deque
 
 def bgr2gray(x):
     return cv.cvtColor(x,cv.COLOR_BGR2GRAY).astype(float)/255
@@ -21,16 +18,10 @@ def history(cam, n, fun = lambda x: x):
         d.appendleft(fun(next(cam)))
         yield np.array(d)
 
+stream = autoStream( lambda x:  history(x, 10, bgr2gray) )
 
-parser = argparse.ArgumentParser()
-sourceArgs(parser)
-args = parser.parse_args()
-
-stream = mkStream(args.size, args.dev)
-
-for h in history(stream, 10, bgr2gray):
-    if cv.waitKey(1) & 0xFF == 27: break
-
+for key, h in stream:
+    
     cv.imshow('input',h[0])
     #print(h.dtype)
     m = np.mean(h,axis=0)
--- code/stream.py
+++ code/stream.py
@@ -4,26 +4,15 @@
 # > ./stream.py --dev=0 --size=800x600
 # > ./stream.py --dev=file:../images/rot4.mjpg
 # > ./stream.py --dev=http://155.54.X.Y:8080/video
-# > ./stream.py --dev=glob:../images/ccorr/scenes/*.png
+# > ./stream.py --dev=glob:../images/ccorr/scenes/*.png --pause
 # > ./stream.py --dev=picam
 
 # > ffmpeg -i video.avi -c:v mjpeg -q:v 3 -huffman optimal -an video.mjpg
 
-
 import cv2          as cv
-import numpy        as np
-import argparse
+from umucv.stream import autoStream
 
-from umucv.stream import mkStream, withKey
-from umucv.util   import sourceArgs
-
-parser = argparse.ArgumentParser()
-sourceArgs(parser)
-args = parser.parse_args()
-
-stream = mkStream(args.size, args.dev)
-
-for key,frame in withKey(stream):
+for key,frame in autoStream():
     cv.imshow('input',frame)
 
 cv.destroyAllWindows()
