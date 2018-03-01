074764dafff2572112eb4af8756520e37aa90215
Alberto Ruiz <aruiz@um.es>
2018-03-01 18:00:21 +0100

    deque.py más simple

--- code/deque.py
+++ code/deque.py
@@ -1,32 +1,21 @@
 #!/usr/bin/env python
 
-# > ./deque.py --dev=file:../images/rot4.mjpg
+import cv2   as cv
+import numpy as np
+from umucv.stream import autoStream
 
-import cv2         as cv
-import numpy       as np
-from umucv.stream  import autoStream
-from collections   import  deque
+from collections import deque
+
+d = deque(maxlen=20)
 
 def bgr2gray(x):
     return cv.cvtColor(x,cv.COLOR_BGR2GRAY).astype(float)/255
 
-def history(cam, n, fun = lambda x: x):
-    d = deque(maxlen=n)
-    for k in range(n-1):
-        d.appendleft(fun(next(cam)))
-    while True:
-        d.appendleft(fun(next(cam)))
-        yield np.array(d)
-
-stream = autoStream( lambda x:  history(x, 10, bgr2gray) )
-
-for key, h in stream:
-    
-    cv.imshow('input',h[0])
-    #print(h.dtype)
-    m = np.mean(h,axis=0)
-    #print(m)
-    cv.imshow('ghost', m)
+for key,frame in autoStream():
+    d.appendleft(bgr2gray(frame))
+    cv.imshow('input',d[0])
+    cv.imshow('delay',d[-1])
+    cv.imshow('ghost',np.mean(d,axis=0))
 
 cv.destroyAllWindows()
 
