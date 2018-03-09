6f87198f697fd72a751699c5a8f880a7941edffb
Alberto Ruiz <aruiz@um.es>
2018-03-09 11:15:19 +0100

    textura

---
+++ images/texturas/get.sh
@@ -0,0 +1,9 @@
+wget https://robot.inf.um.es/material/va/texturas.tar.gz
+tar xzf texturas.tar.gz
+mv texturas simple
+rm texturas.tar.gz
+
+wget https://robot.inf.um.es/material/va/curetgrey.zip
+unzip curetgrey.zip
+rm curetgrey.zip
+
