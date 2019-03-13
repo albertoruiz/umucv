c1b25eb8cb933b2b6c0ffb04d2879de6aca8708e
Alberto Ruiz <aruiz@um.es>
2019-03-13 15:44:28 +0100

    ejercicio 11

--- code/histogram.py
+++ code/histogram.py
@@ -6,19 +6,27 @@ import matplotlib.pyplot as plt
 
 cap = cv.VideoCapture(0)
 
-while(True):
-    if cv.waitKey(1) & 0xFF == 27: break
+while True:
+    key = cv.waitKey(1) & 0xFF
+    if key == 27: break
 
     ret, frame = cap.read()
-    
-    x = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
-    
-    h,b = np.histogram(x, np.arange(257))
-    
+
+    gray = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+
+    h,b = np.histogram(gray, np.arange(257))
+
     # ajustamos la escala del histograma para que se vea bien en la imagen
-    kk = np.array([2*b[1:], 480-h*(480/10000)]).T.astype(int)
-    cv.polylines(x, [kk], isClosed=False, color=(0,0,255), thickness=2)
-    cv.imshow('histogram',x)
+    # usaremos cv.polylines, que admite una lista de listas de puntos x,y enteros
+    # las coordenadas x son los bins del histograma (quitando el primero)
+    # y las coordenadas y son el propio histograma escalado y desplazado
+    xs = 2*b[1:]
+    ys = 480-h*(480/10000)
+    # trasponemos el array para emparejar cada x e y
+    xys = np.array([xs,ys]).T.astype(int)
+
+    cv.polylines(gray, [xys], isClosed=False, color=0, thickness=2)
+    cv.imshow('histogram',gray)
 
 cv.destroyAllWindows()
 
--- code/webcam.py
+++ code/webcam.py
@@ -3,9 +3,18 @@
 import cv2 as cv
 
 cap = cv.VideoCapture(0)
+assert cap.isOpened(), "conecta la webcam"
+w   = cap.get(cv.CAP_PROP_FRAME_WIDTH)
+h   = cap.get(cv.CAP_PROP_FRAME_HEIGHT)
+fps = cap.get(cv.CAP_PROP_FPS)
 
-while(cv.waitKey(1) & 0xFF != 27):
-    ret, frame = cap.read()
+print(f'{w:.0f}x{h:.0f} {fps}fps')
+
+while True:
+    key = cv.waitKey(1) & 0xFF
+    if key == 27: break
+    
+    ok, frame = cap.read()
     cv.imshow('webcam',frame)
 
 cv.destroyAllWindows()
