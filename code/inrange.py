2d61cded23d25c4e62f5ee503c9b21864a613dcb
Alberto Ruiz <aruiz@um.es>
2020-02-27 13:36:53 +0100

    inrange.py, mean-shift.py, concept_map

---
+++ code/inrange.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+
+# Este programa tiene 4 objetivos:
+# 1) Ilustrar la construcción de una máscara que indica
+#    la localización de objetos de un color caractéristico,
+#    mediante una sencilla umbralización.
+# 2) Comprobar que el espacio de color HSV es útil para esto.
+# 3) Separar diferentes objetos mediante componentes conexas
+#    obteniendo también su bounding box, area y centro.
+# 4) Realizar la misma separación mediante extracción de
+#    contornos.
+
+# python inrange.py --dev=dir:../images/naranjas/*.jpg --resize=0x400
+
+import cv2 as cv
+import numpy as np
+from umucv.stream import autoStream
+
+
+# Creamos sliders para elegir interactivamente los umbrales
+# de segmentación.
+
+# umbrales iniciales
+h1, s1, v1, h2, s2, v2 = 6,71,0,26,255,255
+
+cv.namedWindow("mask")# , cv.WINDOW_NORMAL)
+
+def nothing(x): pass
+
+cv.createTrackbar("h1", "mask", h1, 180, nothing)
+cv.createTrackbar("h2", "mask", h2, 180, nothing)
+cv.createTrackbar("s1", "mask", s1, 255, nothing)
+cv.createTrackbar("s2", "mask", s2, 255, nothing)
+cv.createTrackbar("v1", "mask", v1, 255, nothing)
+cv.createTrackbar("v2", "mask", v2, 255, nothing)
+
+
+for key, frame in autoStream():
+    cv.imshow('input',frame)
+
+    # Convertimos al espacio de color deseado
+    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    
+    # Leemos los sliders
+    h1 = cv.getTrackbarPos('h1','mask')
+    h2 = cv.getTrackbarPos('h2','mask')
+    s1 = cv.getTrackbarPos('s1','mask')
+    s2 = cv.getTrackbarPos('s2','mask')
+    v1 = cv.getTrackbarPos('v1','mask')
+    v2 = cv.getTrackbarPos('v2','mask')
+    
+    # opencv genera máscaras np.uint8 con el convenio 0 (No) - 255 (Sí)
+    # que se ven Negro - Blanco en un cv.imshow.
+    mask0  = cv.inRange(hsv, (h1,s1,v1), (h2,s2,v2) )
+    cv.imshow('mask', mask0)
+
+    # Si hay pequeños puntos activos de ruido, pueden eliminarse con un
+    # filtro de imagen (lo explicaremos más adelante)
+    mask = cv.morphologyEx( mask0, cv.MORPH_OPEN, np.ones([3,3], np.uint8) )
+    cv.imshow('clean', mask)
+    # dependiendo de la aplicación, puede ser necesario aplicar otros filtros
+    # para rellenar agujeros, etc.
+
+
+    # mask2 = scipy.ndimage.morphology.binary_opening(mask)
+    # Otros módulos de proceso de imagen pueden generar int 0-1
+    # y dependiendo del uso que hagamos puede ser necesario convertirlas
+    
+
+    # Una vez que tenemos la máscara, podemos "extraer" los objetos,
+    # mostrándolos sobre fondo negro. Se puede hacer de varias formas:
+    # multiplicando por la máscara (con valores 0-1, y hay que hace np.expand_dims),
+    # asignando la imagen en otra vacía en la zona válida,
+    # o anulando la zona de fondo:
+    result = frame.copy()
+    result[mask ==0] = (0,0,0)
+    cv.imshow('masked', result)
+   
+    # Lo importante realmente es separar los objetos. Para ello usamos el algoritmo
+    # de componentes conexas, en su variante más completa:    
+    rois = frame.copy()
+    n, cc, st, cen = cv.connectedComponentsWithStats(mask)
+
+    # En principio, podemos mostrar la imagen de etiquetas, pero se verán niveles de gris
+    # muy parecidos. En matplotlib queda bien con las paletas que tiene, con opencv multiplicamos
+    # para separar un poco los tonos de gris:
+    cv.imshow('CC',5*cc.astype(np.uint8))
+
+    # Es mejor dibujar las bounding box, opcionalmente filtrando por área:
+    for x in range(n):
+        if 5**2 < st[x][cv.CC_STAT_AREA] < 50**2:
+            x1 = st[x][cv.CC_STAT_LEFT]
+            y1 = st[x][cv.CC_STAT_TOP]
+            x2 = st[x][cv.CC_STAT_WIDTH] + x1
+            y2 = st[x][cv.CC_STAT_HEIGHT] + y1
+            cv.rectangle(rois, (x1,y1), (x2,y2), (255,0,0))
+    cv.imshow('rois',rois)
+
+
+    # Alternativamente, podemos extraer contornos:
+    contours, _ = cv.findContours( mask, cv.RETR_CCOMP, cv.CHAIN_APPROX_NONE)[-2:]    
+    # Quitamos la dimensión superflua y los contornos muy cortos
+    ok = [ x.reshape(-1,2) for x in contours if len(x) > 10 ]
+
+    otra = frame.copy()
+    # Hay dos formas de dibujar los contornos
+    cv.polylines(otra, ok, isClosed=True, color=(255,0,255))
+    #cv.drawContours(otra, ok, -1 , (255,0,128) )
+    cv.imshow('contours', otra)
+    
+
---
+++ code/mean-shift.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+
+# Tracking de un objeto de colores destacados
+# mediante reproyección de histograma.
+# Implementamos una versión simple de "mean shift"
+# y comparamos con cv.CamShift
+
+# El objeto a seguir se marca con el ratón y
+# se pulsa 't'. Entonces se calcula su histograma
+# color y a partir de ahí se obtiene la reproyección
+# de dicho histograma en la imagen de entrada.
+# La nueva posición del objeto sera el valor medio
+# de la reproyección, pero restringido a la zona
+# cercana a la posición anterior.
+
+import cv2          as cv
+import numpy        as np
+from umucv.stream import autoStream
+from umucv.util   import ROI
+
+# calcula el histograma multidimensional de la imagen
+def hist(x, bins=16):
+    return cv.calcHist([x],     # lista de imágenes
+                       [0,1,2], # canales deseados
+                       None,    # posible máscara
+                       [bins, bins, bins],  # número de cajas
+                       [0,256] + [0,256] + [0,256]) # intervalos en cada canal
+
+
+# selección de región de interés más cómoda que cv.selectROI
+cv.namedWindow('input')
+roi = ROI('input')
+
+H = None
+
+bins = 16
+
+for key,frame in autoStream():
+    
+    if H is not None:
+        # reproyección de histograma
+
+        # 1) separamos los canales y calculamos la posición de sus
+        #    valores en el histograma
+        b,g,r = (frame // (256//bins) ).transpose(2,0,1)
+
+        # 2) obtenemos el valor del histograma en toda la imagen
+        #    aprovechando características de numpy
+        L = H[b,g,r]
+
+        # las imágenes de float se muestran en la escala 0:negro 1:blanco
+        cv.imshow('likelihood', L/L.max())
+        
+        
+        # tracking mediante camshift de opencv
+        term_crit = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 1)
+        elipse, track_window = cv.CamShift(L, track_window, term_crit )
+        cv.ellipse(frame, elipse, (0,128,255), 2)
+        (c,r,w,h) = track_window
+        cv.rectangle(frame, (c,r), (c+w, r+h), (0,128,255), 1)
+        
+
+        # implementación propia de mean shift
+
+        # mostramos la posición anterior 
+        cv.rectangle(frame, (cm-szx, rm-szy), (cm+szx, rm+szy), (255,128,255), 2)
+        
+        # preparamos una máscara de tamaño doble para calcular la media
+        # en un entorno del objeto
+        mask = np.zeros_like(L, np.uint8)
+        s = 2
+        # thickness = -1 rellena el rectángulo
+        cv.rectangle(mask,  (cm-2*szx, rm-2*szy), (cm+2*szx, rm+2*szy), color=255, thickness=-1)
+        cv.imshow('mask', mask)
+        
+        # restringimos la reproyección a esa zona 
+        L = L*mask
+       
+        # calculamos el valor medio ponderado aprovechando de nuevo numpy
+        h,w = L.shape
+        rs = np.arange(h).reshape(-1,1)
+        cs = np.arange(w).reshape(1,-1)
+        sL = np.sum(L)
+        if sL == 0:
+            H = None
+            continue
+        rm = int(np.sum(L*rs) / sL)
+        cm = int(np.sum(L*cs) / sL)
+        #print(rm,cm)
+
+
+    
+    # seleccionamos una región
+    if roi.roi:
+        [x1,y1,x2,y2] = roi.roi
+        
+        if key == ord('t'):
+            trozo = frame[y1:y2+1, x1:x2+1].copy()
+            cv.imshow('trozo', trozo)
+            H = hist(trozo, bins)
+            # suavizado opcional
+            H = cv.GaussianBlur(H,(0,0),1)
+            #print(H.shape)
+            
+            # región de búsqueda inicial para cv.CamShift            
+            c = x1
+            r = y1
+            w = x2-x1
+            h = y2-y1
+            track_window = (c,r,w,h)
+            
+            # reción de búsqueda para nuestro mean shift
+            cm, rm   = (x1+x2)//2 , (y1+y2)//2
+            szx,szy  = (x2-x1)//2 , (y2-y1)//2
+            
+            # quitar el roi cuando se selecciona la región
+            roi.roi = []
+        
+        cv.rectangle(frame, (x1,y1), (x2,y2), color=(0,255,255), thickness=2)
+        
+    #print(roi.roi)
+    
+    cv.imshow('input',frame)
+
+cv.destroyAllWindows()
+
