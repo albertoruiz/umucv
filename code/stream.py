c8f60d70d8e6486a104544990b62372514d25e76
Alberto Ruiz <aruiz@um.es>
2020-03-16 10:07:10 +0100

    Animaciones en Keypoints

--- code/stream.py
+++ code/stream.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python
 
 # > ./stream.py
-# > ./stream.py --dev=0 --size=800x600  --resize=200x0
-# > ./stream.py --dev=file:path/to/video  [--loop]
-# > ./stream.py --dev=http://155.54.X.Y:8080/video
-# > ./stream.py --dev=glob:../images/ccorr/scenes/*.png [--step]
-# > ./stream.py --dev=dir:../images/ccorr/scenes/*.png
-# > ./stream.py --dev=picam
+# > ./stream.py --dev=help
 
 import cv2          as cv
 from umucv.stream import autoStream
