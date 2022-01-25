b7ccbe0f0b662ce49bbf1f9b32ef1c09d3c3a374
Alberto Ruiz <aruiz@um.es>
2022-01-25 11:43:39 +0100

    Enlace alternativo al dataset de dlib

--- data/get.sh
+++ data/get.sh
@@ -1,3 +1,5 @@
 wget http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
 bunzip2 shape_predictor_68_face_landmarks.dat.bz2
 
+# wget https://robot.inf.um.es/material/va/shape_predictor_68_face_landmarks.dat
+
