667d89a420ec822fc4a3ecb0aaff8d0e3f545746
Alberto Ruiz <aruiz@um.es>
2022-03-13 19:54:01 +0100

    Nueva semana
    -mjpegserver
    -shapes/trebol permissions

--- code/mjpegserver.py
+++ code/mjpegserver.py
@@ -8,7 +8,8 @@
 '''
 # https://gist.github.com/n3wtron/4624820
 
-import cv2
+import cv2 as cv
+import numpy as np
 from PIL import Image
 import threading
 from http.server import BaseHTTPRequestHandler,HTTPServer
@@ -16,9 +17,7 @@ from socketserver import ThreadingMixIn
 from io import StringIO,BytesIO
 import time
 import subprocess
-
-
-from umucv.stream import Camera, sourceArgs
+from umucv.stream import Camera, sourceArgs, autoStream
 import signal
 import sys
 import argparse
@@ -26,19 +25,31 @@ import argparse
 parser = argparse.ArgumentParser()
 sourceArgs(parser)
 parser.add_argument('--quality', help='jpeg quality', type=int, default=30)
+parser.add_argument('--preview', help='check process before launching the server', action="store_true")
 args = parser.parse_args()
 
-QUALITY = args.quality
+################################################################################
+
+def process(img):
+    return np.rot90((255-img))
+
+################################################################################
+
+if args.preview:
+    for key, frame in autoStream():
+        cv.imshow("test", process(frame))
+    cv.destroyAllWindows()
+
 
 def encode(frame):
-    imgRGB=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+    imgRGB=cv.cvtColor(frame,cv.COLOR_BGR2RGB)
     jpg = Image.fromarray(imgRGB)
     tmpFile = BytesIO()
-    jpg.save(tmpFile,'JPEG',quality=QUALITY)
+    jpg.save(tmpFile,'JPEG',quality=args.quality)
     #print('encoded', tmpFile.getbuffer().nbytes)
     return tmpFile
 
-cam = Camera(args.size, args.dev, debug=False)
+cam = Camera(debug=False)
 
 stop = False
 
@@ -49,7 +60,6 @@ def signal_handler(signal, frame):
         sys.exit(0)
 
 signal.signal(signal.SIGINT, signal_handler)
-
 
 class CamHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -64,7 +74,7 @@ class CamHandler(BaseHTTPRequestHandler):
                         time.sleep(0.01)
                         ##print('.')
                     t = cam.time
-                    result = 255 - cam.frame
+                    result = process(cam.frame)
                     tmpFile = encode(result)
                     #print('sent',tmpFile.getbuffer().nbytes)
                     self.wfile.write("--jpgboundary\r\n".encode())
@@ -80,7 +90,6 @@ class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
 
 def main():
     try:
-        
         server = ThreadedHTTPServer(('', 8087), CamHandler)
         #ip = subprocess.check_output(["hostname", "-I"]).decode('utf-8')[:-2]
         print(f"server started at http://localhost:8087/cam.mjpg")
