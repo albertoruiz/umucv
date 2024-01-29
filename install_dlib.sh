032075f27551a2884365ce8a3eea2f810510ef3d
Alberto Ruiz <aruiz@um.es>
2024-01-29 19:48:47 +0100

    requirements.txt

--- install_dlib.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-pip install http://robot.inf.um.es/material/va/dlib-19.24.2-cp311-cp311-linux_x86_64.whl
-
--- install_dlib_lab.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-pip install http://robot.inf.um.es/material/va/dlib-19.23.0-cp38-cp38-linux_x86_64_lab.whl
-
