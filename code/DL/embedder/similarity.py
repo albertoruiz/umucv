b5bd28441150f40e7904b3920301f37ecb9825bb
Alberto Ruiz <aruiz@um.es>
2024-03-05 11:42:14 +0100

    ejercicios

---
+++ code/DL/embedder/similarity.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+# https://github.com/googlesamples/mediapipe/blob/main/examples/image_embedder/python/image_embedder.ipynb
+
+# !wget -O embedder.tflite -q https://storage.googleapis.com/mediapipe-models/image_embedder/mobilenet_v3_small/float32/1/mobilenet_v3_small.tflite
+
+
+import cv2          as cv
+from umucv.stream import autoStream
+import math
+import numpy as np
+
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+
+options = vision.ImageEmbedderOptions(
+    base_options = python.BaseOptions(model_asset_path='embedder.tflite'),
+    l2_normalize = True,
+    quantize     = True
+)
+embedder = vision.ImageEmbedder.create_from_options(options)
+
+model = None
+
+for key,frame in autoStream():
+    
+    mpimage = mp.Image(image_format=mp.ImageFormat.SRGB, data=cv.cvtColor(frame, cv.COLOR_BGR2RGB))
+
+    descriptor = embedder.embed(mpimage).embeddings[0]
+    
+    if key==ord('c'):
+        model = descriptor
+    
+    if model is not None:
+        similarity = vision.ImageEmbedder.cosine_similarity(descriptor, model)
+        W = frame.shape[1]
+        cv.rectangle(frame,(0,0),(int(similarity*W), 20), color=(0,255,0), thickness=-1)
+        
+    cv.imshow("similarity", frame)
+
--- code/facemesh.py
+++ code/facemesh.py
@@ -11,6 +11,9 @@ drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 
 # cv.namedWindow('MediaPipe FaceMesh', cv.WINDOW_NORMAL| cv.WINDOW_GUI_NORMAL)
 
+face_mesh = mp_face_mesh.FaceMesh(min_detection_confidence=0.5,
+                                  min_tracking_confidence=0.5)
+
 POINTS = False
 
 for key,frame in autoStream():
@@ -19,29 +22,26 @@ for key,frame in autoStream():
     
     if key==ord("p"):
         POINTS = not POINTS
-    
-    with mp_face_mesh.FaceMesh(min_detection_confidence=0.5,
-                               min_tracking_confidence=0.5) as face_mesh:
-        
-        image = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-        results = face_mesh.process(image)
+            
+    image = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+    results = face_mesh.process(image)
 
-        if results.multi_face_landmarks:
-            for face_landmarks in results.multi_face_landmarks:
-                
-                if POINTS:
-                    for p, lan in enumerate(face_landmarks.landmark):
-                        x=int(lan.x*w)
-                        y=int(lan.y*h)
-                        putText(frame,str(p), orig=(x,y), div=1, scale=0.5)
+    if results.multi_face_landmarks:
+        for face_landmarks in results.multi_face_landmarks:
+            
+            if POINTS:
+                for p, lan in enumerate(face_landmarks.landmark):
+                    x=int(lan.x*w)
+                    y=int(lan.y*h)
+                    putText(frame,str(p), orig=(x,y), div=1, scale=0.5)
 
-                else:                
-                    mp_drawing.draw_landmarks(
-                        image=frame,
-                        landmark_list=face_landmarks,
-                        connections=mp_face_mesh.FACEMESH_CONTOURS,
-                        landmark_drawing_spec=drawing_spec,
-                        connection_drawing_spec=drawing_spec)
+            else:                
+                mp_drawing.draw_landmarks(
+                    image=frame,
+                    landmark_list=face_landmarks,
+                    connections=mp_face_mesh.FACEMESH_CONTOURS,
+                    landmark_drawing_spec=drawing_spec,
+                    connection_drawing_spec=drawing_spec)
     
     cv.imshow('MediaPipe FaceMesh', frame)
 
