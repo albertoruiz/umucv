db2f99e9c2cb598b047b8f44cf00d008e3780770
Alberto Ruiz <aruiz@um.es>
2025-03-05 09:10:33 +0100

    Guion sesión práctica 6

--- data/get.sh
+++ data/get.sh
@@ -1,5 +1,3 @@
 wget http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
 bunzip2 shape_predictor_68_face_landmarks.dat.bz2
 
-# wget https://robot.inf.um.es/material/va/shape_predictor_68_face_landmarks.dat
-
